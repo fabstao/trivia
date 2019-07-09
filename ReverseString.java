4fe5c2841fdeeed02c6c9afe6b33b3431ac0d39b
fabstao <fabian@nuo.com.mx>
2019-07-08 22:31:22 -0500

    Added trivia 1, 3 exercises

--- ReverseString.java
+++ ReverseString.java
@@ -1,3 +1,7 @@
+/* 
+    Usage:
+    ${JAVA_HOME}/bin/java ReverseString <string>
+*/
 public class ReverseString {
 	public static void main(String[] args) {
         System.out.println("***************************");
